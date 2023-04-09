dd3296c0694c47ea66e4042c6e50616b5d9eb9bd
Dan Dexter <dan.e.dexter@nasa.gov>
2023-04-08 19:01:07 -0500

    Resolved strncpy warning.

--- source/SpaceFOM/RefFrameBase.cpp
+++ source/SpaceFOM/RefFrameBase.cpp
@@ -386,10 +386,10 @@ void RefFrameBase::unpack()
 char *RefFrameBase::allocate_input_string( // RETURN: -- None.
    char const *c_string )                  // IN: -- String to allocate.
 {
-   char *new_c_str;
+   string str = (c_string != NULL) ? c_string : "";
 
-   new_c_str = (char *)TMM_declare_var_1d( "char", strlen( c_string ) + 1 );
-   strncpy( new_c_str, c_string, strlen( c_string ) + 1 );
+   char * new_c_str = (char *)TMM_declare_var_1d( "char", str.length() + 1 );
+   strncpy( new_c_str, c_string, str.length() + 1 );
 
    return new_c_str;
 }
@@ -400,9 +400,7 @@ char *RefFrameBase::allocate_input_string( // RETURN: -- None.
 char *RefFrameBase::allocate_input_string( // RETURN: -- None.
    string const &cpp_string )              // IN: -- String to allocate.
 {
-   char *new_c_str;
-
-   new_c_str = (char *)TMM_declare_var_1d( "char", cpp_string.length() + 1 );
+   char * new_c_str = (char *)TMM_declare_var_1d( "char", cpp_string.length() + 1 );
    strncpy( new_c_str, cpp_string.c_str(), cpp_string.length() + 1 );
 
    return new_c_str;
