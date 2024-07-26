f52a090361612369fdd6bb0e3cd437236d7b5abd
Dan Dexter <dan.e.dexter@nasa.gov>
2024-07-26 15:11:04 -0500

    Added support for new Pitch include path.

--- makefiles/S_hla.mk
+++ makefiles/S_hla.mk
@@ -9,42 +9,79 @@ RTI_HOME      ?= ${HOME}/rti/pRTI1516e
 
 # Make sure the critical environment variable paths we depend on are valid.
 ifeq ("$(wildcard ${TRICKHLA_HOME})","")
-   $(error "Must specify a valid TRICKHLA_HOME environment variable, which is currently set to invalid path ${TRICKHLA_HOME}")
+   $(error S_hla.mk:ERROR: Must specify a valid TRICKHLA_HOME environment variable, which is currently set to invalid path ${TRICKHLA_HOME})
 endif
 ifeq ("$(wildcard ${RTI_HOME})","")
-   $(error "Must specify a valid RTI_HOME environment variable, which is currently set to invalid path ${RTI_HOME}")
+   $(error S_hla.mk:ERROR: Must specify a valid RTI_HOME environment variable, which is currently set to invalid path ${RTI_HOME})
 endif
 
 # Needed for TrickHLA.
 TRICK_SFLAGS   += -I${TRICKHLA_HOME}/S_modules
-TRICK_CFLAGS   += -I${TRICKHLA_HOME}/include -I${TRICKHLA_HOME}/models -DIEEE_1516_2010
-TRICK_CXXFLAGS += -I${TRICKHLA_HOME}/include -I${TRICKHLA_HOME}/models -DIEEE_1516_2010
-
-# The HLA IEEE 1516 header files are c++11 standard compliant.
-TRICK_ICGFLAGS += --icg-std=c++11
+TRICK_CFLAGS   += -I${TRICKHLA_HOME}/include -I${TRICKHLA_HOME}/models -DIEEE_1516_2010 -Wno-deprecated-declarations
+TRICK_CXXFLAGS += -I${TRICKHLA_HOME}/include -I${TRICKHLA_HOME}/models -DIEEE_1516_2010 -Wno-deprecated-declarations
 
 # Needed for the HLA IEEE 1516 header files.
 ifeq ($(RTI_VENDOR),Pitch_HLA_Evolved)
-   TRICK_CFLAGS   += -I${RTI_HOME}/include
-   TRICK_CXXFLAGS += -I${RTI_HOME}/include
-else ifeq ($(RTI_VENDOR),Mak)
-   TRICK_CFLAGS   += -DRTI_VENDOR=Mak_HLA_Evolved -I${RTI_HOME}/include/HLA1516E
-   TRICK_CXXFLAGS += -DRTI_VENDOR=Mak_HLA_Evolved -I${RTI_HOME}/include/HLA1516E
+   # Determine the Pitch RTI include path.
+   RTI_INCLUDE = ${RTI_HOME}/api/cpp/HLA_1516-2010
+   ifeq ("$(wildcard ${RTI_INCLUDE})","")
+      RTI_INCLUDE = ${RTI_HOME}/include
+   endif
+   TRICK_CFLAGS   += -I${RTI_INCLUDE}
+   TRICK_CXXFLAGS += -I${RTI_INCLUDE}
 else ifeq ($(RTI_VENDOR),Mak_HLA_Evolved)
-   TRICK_CFLAGS   += -DRTI_VENDOR=Mak_HLA_Evolved -I${RTI_HOME}/include/HLA1516E
-   TRICK_CXXFLAGS += -DRTI_VENDOR=Mak_HLA_Evolved -I${RTI_HOME}/include/HLA1516E
+   RTI_INCLUDE    =  ${RTI_HOME}/include/HLA1516E
+   TRICK_CFLAGS   += -DRTI_VENDOR=Mak_HLA_Evolved -I${RTI_INCLUDE}
+   TRICK_CXXFLAGS += -DRTI_VENDOR=Mak_HLA_Evolved -I${RTI_INCLUDE}
 else
-   $(error "Unsupported RTI_VENDOR '${RTI_VENDOR}', must specify one of Pitch_HLA_Evolved, Mak, or Mak_HLA_Evolved.")
+   $(error S_hla.mk:ERROR: Unsupported RTI_VENDOR '${RTI_VENDOR}', must specify one of Pitch_HLA_Evolved or Mak_HLA_Evolved.)
+endif
+
+# Configure the ICG and swig excludes.
+ifdef TRICK_ICG_EXCLUDE
+   TRICK_ICG_EXCLUDE += :${RTI_INCLUDE}
+else
+   TRICK_ICG_EXCLUDE = ${RTI_INCLUDE}
+endif
+ifdef TRICK_SWIG_EXCLUDE
+   TRICK_SWIG_EXCLUDE += :${RTI_INCLUDE}
+else
+   TRICK_SWIG_EXCLUDE = ${RTI_INCLUDE}
+endif
+
+# Determine what compiler Trick is using.
+ifneq (,$(findstring trick-gte, $(shell which trick-gte)))
+   CPPC_CMD = $(shell trick-gte TRICK_CXX)
+   ifeq (,$(CPPC_CMD))
+      CPPC_CMD = $(shell trick-gte TRICK_CPPC)
+      ifeq (,$(CPPC_CMD))
+         $(error S_hla.mk:ERROR: Could not determine compiler from TRICK_CXX or TRICK_CPPC using trick-gte command!)
+      endif
+   endif
+else
+   CPPC_CMD = $(shell gte TRICK_CXX)
+   ifeq (,$(CPPC_CMD))
+      CPPC_CMD = $(shell gte TRICK_CPPC)
+      ifeq (,$(CPPC_CMD))
+         $(error S_hla.mk:ERROR: Could not determine compiler from TRICK_CXX or TRICK_CPPC using Trick gte command!)
+      endif
+   endif
 endif
 
 ifeq ($(TRICK_HOST_TYPE),Darwin)
    # macOS
 
+   # C++17 removed the dynamic exception specification so fallback to C++14 for
+   # ICG because the IEEE 1516-2010 APIs use dynamic exception specifications.
+   # Otherwise this will result in compile time errors.
+   TRICK_ICGFLAGS += --icg-std=c++14
+   $(info S_hla.mk:INFO: Using C++14 for Trick ICG code.)
+
    ifeq ($(RTI_VENDOR),Pitch_HLA_Evolved)
       # Allow the user to override RTI_JAVA_HOME or RTI_JAVA_LIB_PATH,
       # otherwise we provide defaults.
       ifdef RTI_JAVA_HOME
-         $(info "User defined RTI_JAVA_HOME = ${RTI_JAVA_HOME}")
+         $(info S_hla.mk:INFO: User defined RTI_JAVA_HOME = ${RTI_JAVA_HOME})
       endif
       RTI_JAVA_HOME ?= ${RTI_HOME}/jre
       ifneq ("$(wildcard ${RTI_JAVA_HOME}/jre/lib/server)","")
@@ -56,30 +93,19 @@ ifeq ($(TRICK_HOST_TYPE),Darwin)
 
       # Verify the RTI Java Home and Lib paths.
       ifeq ("$(wildcard ${RTI_JAVA_HOME})","")
-         $(error "The path specified by RTI_JAVA_HOME is invalid for ${RTI_JAVA_HOME}")
+         $(error S_hla.mk:ERROR: The path specified by RTI_JAVA_HOME is invalid for ${RTI_JAVA_HOME})
       endif
       ifeq ("$(wildcard ${RTI_JAVA_LIB_PATH})","")
-         $(error "The path specified by RTI_JAVA_LIB_PATH is invalid for ${RTI_JAVA_LIB_PATH}")
+         $(error S_hla.mk:ERROR: The path specified by RTI_JAVA_LIB_PATH is invalid for ${RTI_JAVA_LIB_PATH})
       endif
 
-      # Determine what compiler Trick is using to figure out the correct
-      # Pitch library to use.
-      ifneq (,$(findstring trick-gte, $(shell which trick-gte)))
-         CPPC_CMD = $(shell trick-gte TRICK_CXX)
-         ifeq (,$(CPPC_CMD))
-            CPPC_CMD = $(shell trick-gte TRICK_CPPC)
-         endif
-      else
-         CPPC_CMD = $(shell gte TRICK_CXX)
-         ifeq (,$(CPPC_CMD))
-            CPPC_CMD = $(shell gte TRICK_CPPC)
-         endif
-      endif
+      # Determine if the compiler is clang or gcc.
       ifneq (,$(findstring clang, $(shell $(CPPC_CMD) --version | grep clang)))
-         # Determine what clang library version to use.
+
+         # Determine the clang version.
          COMPILER_VERSION = $(shell $(CPPC_CMD) --version | grep clang | cut -d' ' -f 4 | cut -d . -f 1)
          ifneq ("$(wildcard ${RTI_HOME}/lib/clang12)","")
-            # The clang12 library exists but is the compiler at least version 12.
+            # clang12 library exists and is the clang compiler at least version 12.
             COMPILER_GTE_12 = $(shell echo $(COMPILER_VERSION)\>=12 | bc )
          else
             COMPILER_GTE_12 = 0
@@ -113,17 +139,39 @@ ifeq ($(TRICK_HOST_TYPE),Darwin)
       export CLASSPATH += ${RTI_HOME}/lib/prti1516e.jar
       export TRICK_GTE_EXT += CLASSPATH DYLD_LIBRARY_PATH
    else
-      $(error "Unsupported RTI_VENDOR '${RTI_VENDOR}', must specify Pitch_HLA_Evolved.")
+      $(error S_hla.mk:ERROR: Unsupported RTI_VENDOR '${RTI_VENDOR}', must specify Pitch_HLA_Evolved.)
    endif
 
 else
    # Linux
 
+   # Determine the gcc compiler version.
+   COMPILER_VERSION = $(shell $(CPPC_CMD) -dumpversion | cut -d . -f 1)
+
+   # The gcc version 11 compiler defaults to C++17 which removed the
+   # dynamic exception specification. Instead fallback to C++14 because
+   # the IEEE 1516-2010 APIs use dynamic exception specifications.
+   # Otherwise this will result in compile time errors for C++17.
+   ifeq ($(shell echo $(COMPILER_VERSION)\>=11 | bc),1)
+      TRICK_CFLAGS   += -std=c++14
+      TRICK_CXXFLAGS += -std=c++14
+      $(info S_hla.mk:INFO: Falling back to C++14 to compile Trick simulation.)
+   endif
+
+   # ICG code needs to be targeted to either C++14 (gcc versions 6.1 to 10)
+   # or C++11 (gcc 4.8.1+) because C++17 (gcc version 11+) removed the dynamic
+   # exception specification and the IEEE 1516-2010 APIs use it. Otherwise
+   # this will result in compile time errors.
+   ifeq ($(shell echo $(COMPILER_VERSION)\>=6 | bc),1)
+      TRICK_ICGFLAGS += --icg-std=c++14
+      $(info S_hla.mk:INFO: Using C++14 for Trick ICG code.)
+   endif
+
    ifeq ($(RTI_VENDOR),Pitch_HLA_Evolved)
       # Allow the user to override RTI_JAVA_HOME or RTI_JAVA_LIB_PATH,
       # otherwise we provide defaults.
       ifdef RTI_JAVA_HOME
-         $(info "User defined RTI_JAVA_HOME = ${RTI_JAVA_HOME}")
+         $(info S_hla.mk:INFO: User defined RTI_JAVA_HOME = ${RTI_JAVA_HOME})
       endif
       RTI_JAVA_HOME ?= ${RTI_HOME}/jre
       ifneq ("$(wildcard ${RTI_JAVA_HOME}/jre/lib/amd64/server)","")
@@ -138,54 +186,23 @@ else
       endif
       # Verify the RTI Java Home and Lib paths.
       ifeq ("$(wildcard ${RTI_JAVA_HOME})","")
-         $(error "The path specified by RTI_JAVA_HOME is invalid for ${RTI_JAVA_HOME}")
+         $(error S_hla.mk:ERROR: The path specified by RTI_JAVA_HOME is invalid for ${RTI_JAVA_HOME})
       endif
       ifeq ("$(wildcard ${RTI_JAVA_LIB_PATH})","")
-         $(error "The path specified by RTI_JAVA_LIB_PATH is invalid for ${RTI_JAVA_LIB_PATH}")
+         $(error S_hla.mk:ERROR: The path specified by RTI_JAVA_LIB_PATH is invalid for ${RTI_JAVA_LIB_PATH})
       endif
       TRICK_USER_LINK_LIBS += -L${RTI_JAVA_LIB_PATH}/.. -L${RTI_JAVA_LIB_PATH} -ljava -ljvm -lverify -Wl,-rpath,${RTI_JAVA_LIB_PATH}/.. -Wl,-rpath,${RTI_JAVA_LIB_PATH}
 
       # Add the CLASSPATH environment variable to the simulation executable.
-      export CLASSPATH += ${RTI_HOME}/lib/prti1516e.jar
+      export CLASSPATH     += ${RTI_HOME}/lib/prti1516e.jar
       export TRICK_GTE_EXT += CLASSPATH
 
-      # Determine what compiler Trick is using to figure out the correct
-      # Pitch library to use.
-      ifneq (,$(findstring trick-gte, $(shell which trick-gte)))
-         CPPC_CMD = $(shell trick-gte TRICK_CXX)
-         ifeq (,$(CPPC_CMD))
-            CPPC_CMD = $(shell trick-gte TRICK_CPPC)
-         endif
-      else
-         CPPC_CMD = $(shell gte TRICK_CXX)
-         ifeq (,$(CPPC_CMD))
-            CPPC_CMD = $(shell gte TRICK_CPPC)
-         endif
-      endif
-
-      # Determine what gcc library version to use.
-      COMPILER_VERSION = $(shell $(CPPC_CMD) -dumpversion | cut -d . -f 1)
-      ifneq ("$(wildcard ${RTI_HOME}/lib/gcc73_64)","")
-         # The gcc73_64 library exists but is the compiler at least version 7.
-         COMPILER_GTE_7 = $(shell echo $(COMPILER_VERSION)\>=7 | bc )
-      else
-         COMPILER_GTE_7 = 0
-      endif
-      ifneq ("$(wildcard ${RTI_HOME}/lib/gcc52_64)","")
-         COMPILER_GTE_5 = $(shell echo $(COMPILER_VERSION)\>=5 | bc )
-      else
-         COMPILER_GTE_5 = 0
-      endif
-      ifneq ("$(wildcard ${RTI_HOME}/lib/gcc41_64)","")
-         COMPILER_GTE_4 = $(shell echo $(COMPILER_VERSION)\>=4 | bc )
-      else
-         COMPILER_GTE_4 = 0
-      endif
-      ifeq ($(COMPILER_GTE_7),1)
+      # Determine which gcc library version to use.
+      ifeq ($(shell echo $(COMPILER_VERSION)\>=7 | bc),1)
          RTI_LIB_PATH = ${RTI_HOME}/lib/gcc73_64
-      else ifeq ($(COMPILER_GTE_5),1)
+      else ifeq ($(shell echo $(COMPILER_VERSION)\>=5 | bc),1)
          RTI_LIB_PATH = ${RTI_HOME}/lib/gcc52_64
-      else ifeq ($(COMPILER_GTE_4),1)
+      else ifeq ($(shell echo $(COMPILER_VERSION)\>=4 | bc),1)
          RTI_LIB_PATH = ${RTI_HOME}/lib/gcc41_64
       else
          RTI_LIB_PATH = ${RTI_HOME}/lib/gcc34_64
@@ -195,27 +212,13 @@ else
       # On Ubuntu, the user needs to add the LD_LIBRARY_PATH shown below to
       # their environment.
       ifneq ("$(wildcard /etc/lsb-release)","")
-        $(info "Add this to your .bashrc file: export LD_LIBRARY_PATH=${RTI_JAVA_LIB_PATH}/..:${RTI_JAVA_LIB_PATH}:${RTI_LIB_PATH}")
+        $(info S_hla.mk:INFO: Add this to your .bashrc file: export LD_LIBRARY_PATH=${RTI_JAVA_LIB_PATH}/..:${RTI_JAVA_LIB_PATH}:${RTI_LIB_PATH})
       endif
 
-   else ifeq ($(RTI_VENDOR),Mak)
-      TRICK_USER_LINK_LIBS += -L${RTI_HOME}/lib -lrti1516e64 -lfedtime1516e64
    else ifeq ($(RTI_VENDOR),Mak_HLA_Evolved)
       TRICK_USER_LINK_LIBS += -L${RTI_HOME}/lib -lrti1516e64 -lfedtime1516e64
    else
-      $(error "Unsupported RTI_VENDOR '${RTI_VENDOR}', must specify one of Pitch_HLA_Evolved, Mak, or Mak_HLA_Evolved.")
+      $(error S_hla.mk:ERROR: Unsupported RTI_VENDOR '${RTI_VENDOR}', must specify one of Pitch_HLA_Evolved or Mak_HLA_Evolved.)
    endif
 
-endif
-
-ifdef TRICK_ICG_EXCLUDE
-   TRICK_ICG_EXCLUDE += :${RTI_HOME}/include
-else
-   TRICK_ICG_EXCLUDE = ${RTI_HOME}/include
-endif
-
-ifdef TRICK_SWIG_EXCLUDE
-   TRICK_SWIG_EXCLUDE += :${RTI_HOME}/include
-else
-   TRICK_SWIG_EXCLUDE = ${RTI_HOME}/include
 endif
