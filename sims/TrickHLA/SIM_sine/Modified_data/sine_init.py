907fa402b1b627454f745cf6b53ca676b2f4595b
Dan Dexter <dan.e.dexter@nasa.gov>
2024-10-01 20:28:37 -0500

    Added sim-objects for zero-lookahead data.

--- sims/TrickHLA/SIM_sine/Modified_data/sine_init.py
+++ sims/TrickHLA/SIM_sine/Modified_data/sine_init.py
@@ -14,6 +14,7 @@
 ##############################################################################
 
 # Analytic sine data
+A.sim_data.time  = 0.0
 A.sim_data.value = 0.0
 A.sim_data.dvdt  = 0.392699
 A.sim_data.amp   = 2.0
@@ -21,14 +22,15 @@ A.sim_data.phase = 0.0
 A.sim_data.freq  = 0.1963495
 
 # Analytic sine truth state
+A.truth_data.time  = 0.0
 A.truth_data.value = 0.0
 A.truth_data.dvdt  = 0.392699
 A.truth_data.amp   = 2.0
 A.truth_data.phase = 0.0
 A.truth_data.freq  = 0.1963495
 
-
 # Propagated sine data
+P.sim_data.time  = 0.0
 P.sim_data.value = 0.0
 P.sim_data.dvdt  = 0.392699
 P.sim_data.amp   = 1.0
@@ -36,6 +38,7 @@ P.sim_data.phase = 0.0
 P.sim_data.freq  = 0.392699
 
 # Propagated sine truth state
+P.truth_data.time  = 0.0
 P.truth_data.value = 0.0
 P.truth_data.dvdt  = 0.392699
 P.truth_data.amp   = 1.0
