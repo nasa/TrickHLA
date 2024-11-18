54ead533debcc4d2fb0ff06689f141981393145b
Dan Dexter <dan.e.dexter@nasa.gov>
2024-11-17 22:27:39 -0600

    Updated documented LCTS and lookahead constraint equations.

--- source/SpaceFOM/ExecutionConfiguration.cpp
+++ source/SpaceFOM/ExecutionConfiguration.cpp
@@ -355,10 +355,17 @@ void ExecutionConfiguration::unpack()
    }
 
    // The following relationships between the Trick real-time software-frame,
-   // Least Common Time Step (LCTS), and lookahead times must hold True:
-   // ( software_frame > 0 ) && ( LCTS > 0 ) && ( lookahead >= 0 )
-   // ( LCTS >= software_frame) && ( LCTS % software_frame == 0 )
-   // ( LCTS >= lookahead ) && ( ( lookahead > 0 ) && ( LCTS % lookahead == 0 ) )
+   // Least Common Time Step (LCTS), and lookahead times must hold True and
+   // we advance HLA logical time with a (dt == lookahead) time step:
+   // ( software_frame > 0 ) && ( LCTS > 0 ) && ( lookahead > 0 )
+   // ( LCTS >= software_frame ) && ( LCTS % software_frame == 0 )
+   // ( LCTS >= lookahead ) && ( LCTS % lookahead == 0 )
+   //
+   // Otherwise, when using zero lookahead (i.e. lookahead == 0) we advance
+   // the HLA logical time with a dt time step:
+   // ( software_frame > 0 ) && ( LCTS > 0 ) && ( dt > 0 )
+   // ( LCTS >= software_frame ) && ( LCTS % software_frame == 0 )
+   // ( LCTS >= dt ) && ( LCTS % dt == 0 )
 
    // Do a bounds check on the least common time step.
    if ( least_common_time_step <= 0 ) {
