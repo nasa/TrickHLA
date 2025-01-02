4f19dd8562ba066d2ce33d824b4cddebb2555785
Dan Dexter <dan.e.dexter@nasa.gov>
2025-01-01 23:18:04 -0600

    Fixed missing includes and changed scope of local variable.

--- models/Wheelbot/Motor/src/ServoSpeedController.cpp
+++ models/Wheelbot/Motor/src/ServoSpeedController.cpp
@@ -1,5 +1,8 @@
-#include "../include/ServoSpeedController.hh"
+#include <iostream>
+#include <math.h>
+
 #include "../include/ServoMotor.hh"
+#include "../include/ServoSpeedController.hh"
 
 using namespace TrickHLAModel;
 
@@ -8,40 +11,39 @@ ServoSpeedController::ServoSpeedController(
    char        side )
    : servo( servo_motor ),
      _side( side ),
-     commanded_speed( 0.0 ),
      _pulse_width( 0.0 )
 {
-   return;
+   this->commanded_speed = 0.0;
 }
 
-void ServoSpeedController::set_commanded_speed( double commandedSpeed )
+void ServoSpeedController::set_commanded_speed( double commanded_speed )
 {
-   double pulse_width;
+   this->commanded_speed = commanded_speed;
 
    if ( _side == 'L' ) {
-      std::cout << "The left wheel desired speed is: " << desired_speed << std::endl;
+      std::cout << "The left wheel desired speed is: " << commanded_speed << std::endl;
 
-      pulse_width = 9.1296697267545980e+001 * pow( desired_speed, 0 )
-                    + 1.3551549019843796e+000 * pow( desired_speed, 1 )
-                    + -2.5748263162935388e-002 * pow( desired_speed, 2 )
-                    + -3.7691759514032080e-003 * pow( desired_speed, 3 )
-                    + 3.8490572015823302e-004 * pow( desired_speed, 4 )
-                    + 4.5526955758039407e-005 * pow( desired_speed, 5 )
-                    + -6.7622608926425730e-007 * pow( desired_speed, 6 );
+      double pulse_width = 9.1296697267545980e+001 * pow( commanded_speed, 0 )
+                           + 1.3551549019843796e+000 * pow( commanded_speed, 1 )
+                           + -2.5748263162935388e-002 * pow( commanded_speed, 2 )
+                           + -3.7691759514032080e-003 * pow( commanded_speed, 3 )
+                           + 3.8490572015823302e-004 * pow( commanded_speed, 4 )
+                           + 4.5526955758039407e-005 * pow( commanded_speed, 5 )
+                           + -6.7622608926425730e-007 * pow( commanded_speed, 6 );
       std::cout << "The pulse width for the side above is " << pulse_width << std::endl;
 
       _pulse_width = (int)pulse_width;
    } else if ( _side == 'R' ) {
 
-      std::cout << "The right wheel desired speed is: " << desired_speed << std::endl;
+      std::cout << "The right wheel desired speed is: " << commanded_speed << std::endl;
 
-      pulse_width = 9.1296697267545980e+001 * pow( desired_speed, 0 )
-                    + -1.3551549019843796e+000 * pow( desired_speed, 1 )
-                    + -2.5748263162935388e-002 * pow( desired_speed, 2 )
-                    + 3.7691759514032080e-003 * pow( desired_speed, 3 )
-                    + 3.8490572015823302e-004 * pow( desired_speed, 4 )
-                    + -4.5526955758039407e-005 * pow( desired_speed, 5 )
-                    + -6.7622608926425730e-007 * pow( desired_speed, 6 );
+      double pulse_width = 9.1296697267545980e+001 * pow( commanded_speed, 0 )
+                           + -1.3551549019843796e+000 * pow( commanded_speed, 1 )
+                           + -2.5748263162935388e-002 * pow( commanded_speed, 2 )
+                           + 3.7691759514032080e-003 * pow( commanded_speed, 3 )
+                           + 3.8490572015823302e-004 * pow( commanded_speed, 4 )
+                           + -4.5526955758039407e-005 * pow( commanded_speed, 5 )
+                           + -6.7622608926425730e-007 * pow( commanded_speed, 6 );
       std::cout << "The pulse width for the side above is " << pulse_width << std::endl;
 
       _pulse_width = (int)pulse_width;
@@ -58,5 +60,4 @@ void ServoSpeedController::set_commanded_speed( double commandedSpeed )
    } else if ( _side == 'R' ) {
       std::cout << "Right Servo angle is: " << _pulse_width << std::endl;
    }
-   // return _pulse_width;
 }
