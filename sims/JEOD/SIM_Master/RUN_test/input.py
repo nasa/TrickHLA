9ab79eca94c10053195a517d255b708df8efa512
Edwin Z. Crues <edwin.z.crues@nasa.gov>
2025-02-21 14:18:20 -0600

    Changed JEOD test date to August 2027.

--- sims/JEOD/SIM_Master/RUN_test/input.py
+++ sims/JEOD/SIM_Master/RUN_test/input.py
@@ -131,9 +131,9 @@ trick.exec_set_stack_trace(True)
 jeod_time.time_manager_init.initializer = "UTC"
 jeod_time.time_manager_init.sim_start_format = trick.TimeEnum.calendar
 
-jeod_time.time_utc.calendar_year   = 2023
-jeod_time.time_utc.calendar_month  =    7
-jeod_time.time_utc.calendar_day    =    4
+jeod_time.time_utc.calendar_year   = 2027
+jeod_time.time_utc.calendar_month  =    8
+jeod_time.time_utc.calendar_day    =   17
 jeod_time.time_utc.calendar_hour   =   12
 jeod_time.time_utc.calendar_minute =    0
 jeod_time.time_utc.calendar_second =  0.0
