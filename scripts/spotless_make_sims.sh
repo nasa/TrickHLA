024f4f72474dc6752afc0927f991e9b42c25a97c
Dan Dexter <dan.e.dexter@nasa.gov>
2025-04-12 21:48:33 -0500

    Added script to build and clean all the example simulations.

---
+++ scripts/spotless_make_sims.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Script to cleanly make all the TrickHLA simulations.
+# January 2, 2025: Dan Dexter/dan.e.dexter@nasa.gov
+#
+
+if [ ! -d $TRICKHLA_HOME ]; then
+   echo "ERROR: TRICKHLA_HOME ${TRICKHLA_HOME} does not exist, exiting..."
+   exit 1
+fi
+echo "TRICKHLA_HOME: ${TRICKHLA_HOME}"
+cd ${TRICKHLA_HOME}
+
+sim_pkgs_list=($(ls ${TRICKHLA_HOME}/sims))
+for sim_pkg in "${sim_pkgs_list[@]}"
+do
+   sim_list=($(ls ${TRICKHLA_HOME}/sims/${sim_pkg}))
+   for sim in "${sim_list[@]}"
+   do
+      echo "----------------------------------------------------------"
+      echo "sims/${sim_pkg}/${sim}"
+      cd ${TRICKHLA_HOME}/sims/${sim_pkg}/${sim}
+      if [ -f makefile ]; then
+         make spotless
+      fi
+      trick-CP
+      status=$?
+      if [ $status -ne 0 ]; then
+         echo "ERROR"
+         exit 1
+      fi
+   done
+done
