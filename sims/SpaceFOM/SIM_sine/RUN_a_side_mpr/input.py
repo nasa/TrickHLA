f01c0c7b7c8e4452fdf7d19167a80afc8fbb5c53
Dan Dexter <dan.e.dexter@nasa.gov>
2025-06-03 18:45:40 -0500

    Revert

--- sims/SpaceFOM/SIM_sine/RUN_a_side_mpr/input.py
+++ sims/SpaceFOM/SIM_sine/RUN_a_side_mpr/input.py
@@ -15,6 +15,8 @@
 #    (((Edwin Z. Crues) (NASA/ER7) (Jan 2019) (--) (SpaceFOM support and testing.))
 #     ((Dan Dexter) (NASA/ER6) (Mar 2024) (--) (SpaceFOM sine example.)))
 ##############################################################################
+import socket
+import subprocess
 import sys
 sys.path.append( '../../../' )
 
@@ -130,6 +132,30 @@ def parse_command_line():
    return
 
 
+def fix_var_server_source_address():
+   # The Trick variable server uses the local host name without verifying the
+   # IP address it resolves to is actually used by the local host computer.
+   # Verify the IP address and fallback to 127.0.0.1 if we find a discrepancy.
+   # Otherwise the simulation control panel will not successfully connect.
+   try:
+      if ( trick.var_server_get_hostname() == socket.gethostname() ):
+         host_ip_addr = socket.gethostbyname( socket.gethostname() )
+         try:
+            ifconfig_out = subprocess.check_output( ['ifconfig'] ).decode()
+            if ( ifconfig_out.find( host_ip_addr ) < 0 ):
+               print( 'WARNING: Invalid IP address ' + host_ip_addr
+                      + ' resolved for host \'' + trick.var_server_get_hostname()
+                      + '\', setting the variable server source address to 127.0.0.1!' )
+               trick.var_server_set_source_address( '127.0.0.1' )
+         except:
+            return  # Use host source address as is.
+   except ( socket.error, socket.gaierror, socket.herror, socket.timeout ):
+      print( 'WARNING: Problem resolving \'' + trick.var_server_get_hostname()
+             + '\' host name to an address, setting the variable server source address to 127.0.0.1!' )
+      trick.var_server_set_source_address( '127.0.0.1' )
+   return
+
+
 # Default: Don't show usage.
 print_usage = False
 
@@ -173,7 +199,6 @@ trick.exec_set_stack_trace( False )
 trick.var_server_set_port( 7000 )
 trick.sim_control_panel_set_enabled( True )
 
-exec( open( "Modified_data/trick/TrickFixVarServerSourceAddress.py" ).read() )
 fix_var_server_source_address()
 
 #---------------------------------------------
