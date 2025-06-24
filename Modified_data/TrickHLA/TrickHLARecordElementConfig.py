0f4154823e329ae6c764206890cf7110025e5e12
Dan Dexter <dan.e.dexter@nasa.gov>
2025-06-23 22:53:02 -0500

    Refactored number of elements count check.

--- Modified_data/TrickHLA/TrickHLARecordElementConfig.py
+++ Modified_data/TrickHLA/TrickHLARecordElementConfig.py
@@ -46,25 +46,27 @@ class TrickHLARecordElementConfig( object ):
    def initialize_fixed_record( self, thla_elem, input_elem ):
 
       if ( input_elem.rti_encoding == trick.TrickHLA.ENCODING_FIXED_RECORD ):
-         thla_elem.rti_encoding = trick.TrickHLA.ENCODING_FIXED_RECORD
 
-         # Allocate the fixed record elements.
+         thla_elem.rti_encoding  = trick.TrickHLA.ENCODING_FIXED_RECORD
          thla_elem.element_count = len( input_elem.elements )
+
          if ( thla_elem.element_count > 0 ):
+
+            # Allocate the fixed record elements.
             thla_elem.elements = trick.TMM_declare_var_1d( 'TrickHLA::RecordElement',
                                                            thla_elem.element_count )
 
-         # Loop through the fixed record elements and initialize them.
-         for indx in range( 0, thla_elem.element_count ):
-            if ( input_elem.elements[indx].rti_encoding == trick.TrickHLA.ENCODING_FIXED_RECORD ):
-               self.initialize_fixed_record( thla_elem.elements[indx],
-                                             input_elem.elements[indx] )
-            else:
-               thla_elem.elements[indx].rti_encoding = input_elem.elements[indx].rti_encoding
-
-               if ( input_elem.elements[indx].trick_name != None ):
-                  thla_elem.elements[indx].trick_name = str( input_elem.elements[indx].trick_name )
+            # Loop through the fixed record elements and initialize them.
+            for indx in range( 0, thla_elem.element_count ):
+               if ( input_elem.elements[indx].rti_encoding == trick.TrickHLA.ENCODING_FIXED_RECORD ):
+                  self.initialize_fixed_record( thla_elem.elements[indx],
+                                                input_elem.elements[indx] )
                else:
-                  thla_elem.elements[indx].trick_name = str( '' )
+                  thla_elem.elements[indx].rti_encoding = input_elem.elements[indx].rti_encoding
+
+                  if ( input_elem.elements[indx].trick_name != None ):
+                     thla_elem.elements[indx].trick_name = str( input_elem.elements[indx].trick_name )
+                  else:
+                     thla_elem.elements[indx].trick_name = str( '' )
 
       return
