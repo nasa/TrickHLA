d7412a5a656433c252e1891f3c9ac8d2c39b150c
Edwin Z. Crues <edwin.z.crues@nasa.gov>
2025-07-17 13:03:35 -0500

    Added fix_var_server_source_address() to the Python TrickHLAFederateConfig base class.

--- sims/SpaceFOM/SIM_Roles_Test/RUN_Master/input.py
+++ sims/SpaceFOM/SIM_Roles_Test/RUN_Master/input.py
@@ -135,30 +135,6 @@ def parse_command_line():
    return
 
 
-def fix_var_server_source_address():
-   # The Trick variable server uses the local host name without verifying the
-   # IP address it resolves to is actually used by the local host computer.
-   # Verify the IP address and fallback to 127.0.0.1 if we find a discrepancy.
-   # Otherwise the simulation control panel will not successfully connect.
-   try:
-      if ( trick.var_server_get_hostname() == socket.gethostname() ):
-         host_ip_addr = socket.gethostbyname( socket.gethostname() )
-         try:
-            ifconfig_out = subprocess.check_output( ['ifconfig'] ).decode()
-            if ( ifconfig_out.find( host_ip_addr ) < 0 ):
-               print( 'WARNING: Invalid IP address ' + host_ip_addr
-                      + ' resolved for host \'' + trick.var_server_get_hostname()
-                      + '\', setting the variable server source address to 127.0.0.1!' )
-               trick.var_server_set_source_address( '127.0.0.1' )
-         except:
-            return  # Use host source address as is.
-   except ( socket.error, socket.gaierror, socket.herror, socket.timeout ):
-      print( 'WARNING: Problem resolving \'' + trick.var_server_get_hostname()
-             + '\' host name to an address, setting the variable server source address to 127.0.0.1!' )
-      trick.var_server_set_source_address( '127.0.0.1' )
-   return
-
-
 # Default: Don't show usage.
 print_usage = False
 
@@ -202,7 +178,7 @@ trick.exec_set_stack_trace( True )
 trick.sim_control_panel_set_enabled( True )
 trick.var_server_set_port( 7000 )
 
-fix_var_server_source_address()
+#fix_var_server_source_address()
 
 # =========================================================================
 # Set up the HLA interfaces.
@@ -216,6 +192,9 @@ federate = SpaceFOMFederateConfig(
    thla_federation_name = federation_name,
    thla_federate_name   = federate_name,
    thla_enabled         = True )
+
+# Check to see if we need to fix the Trick variable server address.
+federate.fix_var_server_source_address()
 
 # Set the name of the ExCO S_define instance.
 # We do not need to do this since we're using the ExCO default_data job
