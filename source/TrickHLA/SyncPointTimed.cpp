0735d4f1a71e70db503113b52d52a5bc3ca95df4
Dan Dexter <dan.e.dexter@nasa.gov>
2025-12-03 16:27:48 -0600

    Updated header include.

--- source/TrickHLA/SyncPointTimed.cpp
+++ source/TrickHLA/SyncPointTimed.cpp
@@ -37,6 +37,7 @@ NASA, Johnson Space Center\n
 #include "TrickHLA/SyncPoint.hh"
 #include "TrickHLA/SyncPointTimed.hh"
 #include "TrickHLA/Types.hh"
+#include "TrickHLA/time/Int64Time.hh"
 
 // C++11 deprecated dynamic exception specifications for a function so we need
 // to silence the warnings coming from the IEEE 1516 declared functions.
