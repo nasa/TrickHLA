9547675dc4418ece03037adc398e3356f0a54934
Edwin Z. Crues <edwin.z.crues@nasa.gov>
2026-01-29 11:13:35 -0600

    Working on a data recording issue with checkpoint restart.

--- sims/TrickHLA/SIM_Ball/RUN_checkpoint/input.py
+++ sims/TrickHLA/SIM_Ball/RUN_checkpoint/input.py
@@ -1,12 +1,59 @@
+##############################################################################
+#
+# @file RUN_test/input.py
+# @ingroup TrickHLA
+# @brief A configuration input class for SIM_Ball.
+# 
+# This is a Trick Python input file class using the TrickHLA base
+# for configuring the Ball test simulation checkpoint run.
+# 
+# @copyright Copyright 2026 United States Government as represented by the
+# Administrator of the National Aeronautics and Space Administration.
+# No copyright is claimed in the United States under Title 17, U.S. Code.
+# All Other Rights Reserved.
+# 
+# \par<b>Responsible Organization</b>
+# Simulation and Graphics Branch, Mail Code ER7\n
+# Software, Robotics & Simulation Division\n
+# NASA, Johnson Space Center\n
+# # 2101 NASA Parkway, Houston, TX  77058
+# 
+# @python_module{ArtemisFOM}
+#
+# @revs_title
+# @revs_begin
+# @rev_entry{ Edwin Z. Crues, NASA JSC, TrickHLA, January 2026, --, Initial version. }
+# @revs_end
+#
+##############################################################################
 
-# Load in the data recording definition function.
-exec(open("Modified_data/data_record.py").read())
+# Find the TrickHLA home location and append the path.
+trickhla_home = os.environ.get( "TRICKHLA_HOME" )
+if trickhla_home is None:
+   sys.exit( '\033[91m'\
+            +'Environment variable TRICKHLA_HOME is not defined!'\
+            +'\033[0m\n' )
+else:
+   if os.path.isdir( trickhla_home ) is False:
+      sys.exit( '\033[91m'\
+               +'TRICKHLA_HOME not found: '\
+               +trickhla_home\
+               +'\033[0m\n' )
+
+# Append the path to the top level of the top level TrickHLA directory.
+# We need this to locate the TrickHLA_data Python data directory.
+if trickhla_home not in sys.path :
+   sys.path.append( trickhla_home )
 
 # Load in the Trick realtime parameter setting.
 exec(open("Modified_data/realtime.py").read())
 
 # Load in the graphics definition and startup functions.
 exec(open("Modified_data/graphics.py").read())
+
+# Load the Ball State Data Recording Group class.
+#from TrickHLA_data.Trick.TrickDataRecordingGroup import *
+from BallStateDRG import *
 
 #---------------------------------------------------------------------------
 # Set the Trick check point information.
@@ -26,17 +73,17 @@ trick.exec_set_enable_freeze( False )
 trick.exec_set_freeze_command( False )
 trick.add_read(0.0, 'trick.load_checkpoint("RUN_test/chkpnt_5.000000")')
 
-#---------------------------------------------------------------------------
-# Add the data recording groups.
-#---------------------------------------------------------------------------
-# Add Ball 1 to data recording.
-add_dr_group( 'ball1', 'Ball1' )
 
-# Add Ball 2 to data recording.
-add_dr_group( 'ball2', 'Ball2' )
+#---------------------------------------------------------------------------
+# Setup the Data Recording Groups.
+#---------------------------------------------------------------------------
+ball1_drg = BallStateDRG( 'ball1', 'Ball1', 0.1 )
+ball2_drg = BallStateDRG( 'ball2', 'Ball2', 0.1 )
+ball3_drg = BallStateDRG( 'ball3', 'Ball3', 0.1 )
 
-# Add Ball 3 to data recording.
-add_dr_group( 'ball3', 'Ball3' )
+# Initialize all the Data Recording Groups.
+TrickDataRecordingGroup.initialize_groups()
+
 
 #---------------------------------------------------------------------------
 # Set up simulation termination time.
