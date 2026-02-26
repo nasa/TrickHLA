4a1277e8a92d65c5ef06574befd6033d7e67658b
Dan Dexter <dan.e.dexter@nasa.gov>
2026-02-26 12:22:50 -0600

    Added a script to update the Pitch pRTI specific environment variables to use the RTI set by RTI_HOME.

---
+++ scripts/update_Pitch_env.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Script to update the Pitch Environment variables.
+# February 26, 2026: Dan Dexter/dan.e.dexter@nasa.gov
+#
+# To run this script type: source $TRICKHLA_HOME/scripts/update_Pitch_env.sh
+#
+
+if [ ! -d $RTI_HOME ]; then 
+   if [[ -t 0 ]]; then
+      echo "WARNING: RTI_HOME ${RTI_HOME} does not exist."
+   fi
+else
+   # Pitch sets system environment variables, so update them to the RTI being used.
+   if [ -n "${PRTI6_ROOT}" ]; then
+      if [ "$PRTI6_ROOT" != "$RTI_HOME" ]; then
+         export PRTI6_ROOT="${RTI_HOME}"
+      fi
+   fi
+   if [ -n "${PitchRTI_ROOT}" ]; then
+      if [ "$PitchRTI_ROOT" != "$RTI_HOME" ]; then
+         export PitchRTI_ROOT="${RTI_HOME}"
+      fi
+   fi
+   if [ -n "${PRTI1516E_HOME}" ]; then
+      if [ "$PRTI1516E_HOME" != "$RTI_HOME" ]; then
+         export PRTI1516E_HOME="${RTI_HOME}"
+      fi
+   fi
+fi
