f5f9699f02a992d6d244c3117b1fc5ff7d047149
Edwin Z. Crues <edwin.z.crues@nasa.gov>
2026-04-13 08:46:19 -0500

    Fixed error in TrickHLA/SIM_sine/RUN_a_side/input.py file.

--- sims/TrickHLA/SIM_sine/RUN_a_side/input.py
+++ sims/TrickHLA/SIM_sine/RUN_a_side/input.py
@@ -109,11 +109,11 @@ THLA.simple_sim_config.run_duration = run_duration
 
 # TrickHLA Interactions and Parameters.
 interaction_service = THLA.federate.get_interaction_service()
-interaction_service.inter_count  = 3
+interaction_service.inter_count  = 1
 interaction_service.interactions = trick.sim_services.alloc_type( interaction_service.inter_count, 'TrickHLA::Interaction' )
 
 # FIXME: This is a kludge to work around a SWIG/TrickIO bug.
-interaction0 = trick.castAsTrickHLA__Interaction(interaction_service.interactions[0])
+interaction0 = trick.castAsTrickHLA__Interaction( interaction_service.interactions[0] )
 
 #interaction_service.interactions[0].FOM_name    = 'Communication'
 #interaction_service.interactions[0].publish     = True
@@ -124,9 +124,9 @@ interaction0 = trick.castAsTrickHLA__Interaction(interaction_service.interaction
 
 # FIXME: Kludged!
 interaction0.FOM_name    = 'Communication'
-interaction0.publish     = True
-interaction0.subscribe   = False
-interaction0.handler     = A.interaction_handler
+interaction0.publish     = False
+interaction0.subscribe   = True
+interaction0.handler     = P.interaction_handler
 interaction0.param_count = 3
 interaction0.parameters  = trick.sim_services.alloc_type( interaction0.param_count, 'TrickHLA::Parameter' )
 
@@ -144,15 +144,15 @@ interaction0.parameters  = trick.sim_services.alloc_type( interaction0.param_cou
 
 # FIXME: Kludged!
 interaction0.parameters[0].FOM_name     = 'Message'
-interaction0.parameters[0].trick_name   = 'A.interaction_handler.message'
+interaction0.parameters[0].trick_name   = 'P.interaction_handler.message'
 interaction0.parameters[0].rti_encoding = trick.ENCODING_UNICODE_STRING
 
 interaction0.parameters[1].FOM_name     = 'time'
-interaction0.parameters[1].trick_name   = 'A.interaction_handler.time'
+interaction0.parameters[1].trick_name   = 'P.interaction_handler.time'
 interaction0.parameters[1].rti_encoding = trick.ENCODING_LITTLE_ENDIAN
 
 interaction0.parameters[2].FOM_name     = 'year'
-interaction0.parameters[2].trick_name   = 'A.interaction_handler.year'
+interaction0.parameters[2].trick_name   = 'P.interaction_handler.year'
 interaction0.parameters[2].rti_encoding = trick.ENCODING_LITTLE_ENDIAN
 
 
