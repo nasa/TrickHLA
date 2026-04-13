f355bafcf4e712aa25e0c89a85988a7447f137be
Edwin Z. Crues <edwin.z.crues@nasa.gov>
2026-04-13 08:25:20 -0500

    Modified JEOD/SIM_RefFrames to build with the new modifications to Federate.

--- sims/TrickHLA/SIM_sine/RUN_a_side/input.py
+++ sims/TrickHLA/SIM_sine/RUN_a_side/input.py
@@ -109,27 +109,49 @@ THLA.simple_sim_config.run_duration = run_duration
 
 # TrickHLA Interactions and Parameters.
 interaction_service = THLA.federate.get_interaction_service()
-interaction_service.inter_count  = 1
+interaction_service.inter_count  = 3
 interaction_service.interactions = trick.sim_services.alloc_type( interaction_service.inter_count, 'TrickHLA::Interaction' )
 
-interaction_service.interactions[0].FOM_name    = 'Communication'
-interaction_service.interactions[0].publish     = True
-interaction_service.interactions[0].subscribe   = False
-interaction_service.interactions[0].handler     = A.interaction_handler
-interaction_service.interactions[0].param_count = 3
-interaction_service.interactions[0].parameters  = trick.sim_services.alloc_type( interaction_service.interactions[0].param_count, 'TrickHLA::Parameter' )
+# FIXME: This is a kludge to work around a SWIG/TrickIO bug.
+interaction0 = trick.castAsTrickHLA__Interaction(interaction_service.interactions[0])
 
-interaction_service.interactions[0].parameters[0].FOM_name     = 'Message'
-interaction_service.interactions[0].parameters[0].trick_name   = 'A.interaction_handler.message'
-interaction_service.interactions[0].parameters[0].rti_encoding = trick.ENCODING_UNICODE_STRING
+#interaction_service.interactions[0].FOM_name    = 'Communication'
+#interaction_service.interactions[0].publish     = True
+#interaction_service.interactions[0].subscribe   = False
+#interaction_service.interactions[0].handler     = A.interaction_handler
+#interaction_service.interactions[0].param_count = 3
+#interaction_service.interactions[0].parameters  = trick.sim_services.alloc_type( interaction_service.interactions[0].param_count, 'TrickHLA::Parameter' )
+interaction0.FOM_name    = 'Communication'
+interaction0.publish     = True
+interaction0.subscribe   = False
+interaction0.handler     = A.interaction_handler
+interaction0.param_count = 3
+interaction0.parameters  = trick.sim_services.alloc_type( interaction0.param_count, 'TrickHLA::Parameter' )
 
-interaction_service.interactions[0].parameters[1].FOM_name     = 'time'
-interaction_service.interactions[0].parameters[1].trick_name   = 'A.interaction_handler.time'
-interaction_service.interactions[0].parameters[1].rti_encoding = trick.ENCODING_LITTLE_ENDIAN
+#interaction_service.interactions[0].parameters[0].FOM_name     = 'Message'
+#interaction_service.interactions[0].parameters[0].trick_name   = 'A.interaction_handler.message'
+#interaction_service.interactions[0].parameters[0].rti_encoding = trick.ENCODING_UNICODE_STRING
 
-interaction_service.interactions[0].parameters[2].FOM_name     = 'year'
-interaction_service.interactions[0].parameters[2].trick_name   = 'A.interaction_handler.year'
-interaction_service.interactions[0].parameters[2].rti_encoding = trick.ENCODING_LITTLE_ENDIAN
+#interaction_service.interactions[0].parameters[1].FOM_name     = 'time'
+#interaction_service.interactions[0].parameters[1].trick_name   = 'A.interaction_handler.time'
+#interaction_service.interactions[0].parameters[1].rti_encoding = trick.ENCODING_LITTLE_ENDIAN
+
+#interaction_service.interactions[0].parameters[2].FOM_name     = 'year'
+#interaction_service.interactions[0].parameters[2].trick_name   = 'A.interaction_handler.year'
+#interaction_service.interactions[0].parameters[2].rti_encoding = trick.ENCODING_LITTLE_ENDIAN
+
+
+interaction0.parameters[0].FOM_name     = 'Message'
+interaction0.parameters[0].trick_name   = 'A.interaction_handler.message'
+interaction0.parameters[0].rti_encoding = trick.ENCODING_UNICODE_STRING
+
+interaction0.parameters[1].FOM_name     = 'time'
+interaction0.parameters[1].trick_name   = 'A.interaction_handler.time'
+interaction0.parameters[1].rti_encoding = trick.ENCODING_LITTLE_ENDIAN
+
+interaction0.parameters[2].FOM_name     = 'year'
+interaction0.parameters[2].trick_name   = 'A.interaction_handler.year'
+interaction0.parameters[2].rti_encoding = trick.ENCODING_LITTLE_ENDIAN
 
 
 # The Federate has two objects, it publishes one and subscribes to another.
--- source/IMSim/ExecutionConfiguration.cpp
+++ source/IMSim/ExecutionConfiguration.cpp
@@ -204,51 +204,61 @@ void ExecutionConfiguration::configure_attributes()
    this->attributes[0].FOM_name     = trick_MM->mm_strdup( "owner" );
    trick_name_str                   = simconfig_name_str + string( ".owner" );
    this->attributes[0].trick_name   = trick_MM->mm_strdup( trick_name_str.c_str() );
+   this->attributes[0].config       = CONFIG_INITIALIZE_AND_INTERMITTENT;
    this->attributes[0].rti_encoding = ENCODING_UNICODE_STRING;
 
    this->attributes[1].FOM_name     = trick_MM->mm_strdup( "scenario" );
    trick_name_str                   = simconfig_name_str + string( ".scenario" );
    this->attributes[1].trick_name   = trick_MM->mm_strdup( trick_name_str.c_str() );
+   this->attributes[1].config       = CONFIG_INITIALIZE_AND_INTERMITTENT;
    this->attributes[1].rti_encoding = ENCODING_UNICODE_STRING;
 
    this->attributes[2].FOM_name     = trick_MM->mm_strdup( "mode" );
    trick_name_str                   = simconfig_name_str + string( ".mode" );
    this->attributes[2].trick_name   = trick_MM->mm_strdup( trick_name_str.c_str() );
+   this->attributes[2].config       = CONFIG_INITIALIZE_AND_INTERMITTENT;
    this->attributes[2].rti_encoding = ENCODING_UNICODE_STRING;
 
    this->attributes[3].FOM_name     = trick_MM->mm_strdup( "run_duration" );
    trick_name_str                   = simconfig_name_str + string( ".run_duration" );
    this->attributes[3].trick_name   = trick_MM->mm_strdup( trick_name_str.c_str() );
+   this->attributes[3].config       = CONFIG_INITIALIZE_AND_INTERMITTENT;
    this->attributes[3].rti_encoding = ENCODING_LITTLE_ENDIAN;
 
    this->attributes[4].FOM_name     = trick_MM->mm_strdup( "number_of_federates" );
    trick_name_str                   = simconfig_name_str + string( ".number_of_federates" );
    this->attributes[4].trick_name   = trick_MM->mm_strdup( trick_name_str.c_str() );
+   this->attributes[4].config       = CONFIG_INITIALIZE_AND_INTERMITTENT;
    this->attributes[4].rti_encoding = ENCODING_LITTLE_ENDIAN;
 
    this->attributes[5].FOM_name     = trick_MM->mm_strdup( "required_federates" );
    trick_name_str                   = simconfig_name_str + string( ".required_federates" );
    this->attributes[5].trick_name   = trick_MM->mm_strdup( trick_name_str.c_str() );
+   this->attributes[5].config       = CONFIG_INITIALIZE_AND_INTERMITTENT;
    this->attributes[5].rti_encoding = ENCODING_UNICODE_STRING;
 
    this->attributes[6].FOM_name     = trick_MM->mm_strdup( "start_year" );
    trick_name_str                   = simconfig_name_str + string( ".start_year" );
    this->attributes[6].trick_name   = trick_MM->mm_strdup( trick_name_str.c_str() );
+   this->attributes[6].config       = CONFIG_INITIALIZE_AND_INTERMITTENT;
    this->attributes[6].rti_encoding = ENCODING_LITTLE_ENDIAN;
 
    this->attributes[7].FOM_name     = trick_MM->mm_strdup( "start_seconds" );
    trick_name_str                   = simconfig_name_str + string( ".start_seconds" );
    this->attributes[7].trick_name   = trick_MM->mm_strdup( trick_name_str.c_str() );
+   this->attributes[7].config       = CONFIG_INITIALIZE_AND_INTERMITTENT;
    this->attributes[7].rti_encoding = ENCODING_LITTLE_ENDIAN;
 
    this->attributes[8].FOM_name     = trick_MM->mm_strdup( "DUT1" );
    trick_name_str                   = simconfig_name_str + string( ".DUT1" );
    this->attributes[8].trick_name   = trick_MM->mm_strdup( trick_name_str.c_str() );
+   this->attributes[8].config       = CONFIG_INITIALIZE_AND_INTERMITTENT;
    this->attributes[8].rti_encoding = ENCODING_LITTLE_ENDIAN;
 
    this->attributes[9].FOM_name     = trick_MM->mm_strdup( "deltaAT" );
    trick_name_str                   = simconfig_name_str + string( ".deltaAT" );
    this->attributes[9].trick_name   = trick_MM->mm_strdup( trick_name_str.c_str() );
+   this->attributes[9].config       = CONFIG_INITIALIZE_AND_INTERMITTENT;
    this->attributes[9].rti_encoding = ENCODING_LITTLE_ENDIAN;
 }
 
