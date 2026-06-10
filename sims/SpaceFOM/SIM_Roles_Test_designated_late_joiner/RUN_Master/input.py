3bc39fbca8716e11121c610e0631129518069c09
Edwin Z. Crues <edwin.z.crues@nasa.gov>
2026-06-10 09:37:23 -0500

    Updated the local_setting string to be compliant with the current Pitch format.  Specifically, instead of 'crcHost = localhost\n crcPort = 8989' it now uses 'crcHost = localhost\:8989'.

--- sims/SpaceFOM/SIM_Roles_Test_designated_late_joiner/RUN_Master/input.py
+++ sims/SpaceFOM/SIM_Roles_Test_designated_late_joiner/RUN_Master/input.py
@@ -220,7 +220,7 @@ federate.add_known_federate( True, rrfp_name )
 # Configure the CRC.
 #--------------------------------------------------------------------------
 # Pitch specific local settings designator:
-THLA.federate.local_settings = 'crcHost = localhost\n crcPort = 8989'
+THLA.federate.local_settings = 'crcHost = localhost\:8989'
 
 #--------------------------------------------------------------------------
 # Set up federate related time related parameters.
