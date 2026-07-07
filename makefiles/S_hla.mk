a70bc66acba5f8dd386fa169afcdf2ced8654239
Dan Dexter <dan.e.dexter@nasa.gov>
2026-07-07 09:56:11 -0500

    Added missing path seperator.

--- makefiles/S_hla.mk
+++ makefiles/S_hla.mk
@@ -347,12 +347,10 @@ else
 
       ifdef LD_LIBRARY_PATH
          ifneq (,$(findstring ${RTI_LIB_PATH}, $(LD_LIBRARY_PATH)))
-            export LD_LIBRARY_PATH += ${RTI_JAVA_LIB_PATH}/..:${RTI_JAVA_LIB_PATH}:${RTI_LIB_PATH}
-            $(info ${GREEN_TXT}S_hla.mk:INFO: Overriding LD_LIBRARY_PATH = $(LD_LIBRARY_PATH)${RESET_TXT})
+            export LD_LIBRARY_PATH += :${RTI_JAVA_LIB_PATH}/..:${RTI_JAVA_LIB_PATH}:${RTI_LIB_PATH}
          endif
       else
          export LD_LIBRARY_PATH = ${RTI_JAVA_LIB_PATH}/..:${RTI_JAVA_LIB_PATH}:${RTI_LIB_PATH}
-         $(info ${GREEN_TXT}S_hla.mk:INFO: Overriding LD_LIBRARY_PATH = $(LD_LIBRARY_PATH)${RESET_TXT})
       endif
 
       # On Ubuntu, the user needs to add the LD_LIBRARY_PATH shown below to
@@ -407,12 +405,10 @@ else
 
       ifdef LD_LIBRARY_PATH
          ifneq (,$(findstring ${RTI_LIB_PATH}, $(LD_LIBRARY_PATH)))
-            export LD_LIBRARY_PATH += ${RTI_JAVA_LIB_PATH}/..:${RTI_JAVA_LIB_PATH}:${RTI_LIB_PATH}
-            $(info ${GREEN_TXT}S_hla.mk:INFO: Overriding LD_LIBRARY_PATH = $(LD_LIBRARY_PATH)${RESET_TXT})
+            export LD_LIBRARY_PATH += :${RTI_JAVA_LIB_PATH}/..:${RTI_JAVA_LIB_PATH}:${RTI_LIB_PATH}
          endif
       else
          export LD_LIBRARY_PATH = ${RTI_JAVA_LIB_PATH}/..:${RTI_JAVA_LIB_PATH}:${RTI_LIB_PATH}
-         $(info ${GREEN_TXT}S_hla.mk:INFO: Overriding LD_LIBRARY_PATH = $(LD_LIBRARY_PATH)${RESET_TXT})
       endif
 
       # On Ubuntu, the user needs to add the LD_LIBRARY_PATH shown below to
