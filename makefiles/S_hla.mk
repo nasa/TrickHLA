26877152733b42436f2c40f3119a952e2c42ba70
Dan Dexter <dan.e.dexter@nasa.gov>
2026-07-13 17:53:49 -0500

    Added check for Ubuntu

--- makefiles/S_hla.mk
+++ makefiles/S_hla.mk
@@ -359,9 +359,11 @@ else
          export LD_LIBRARY_PATH = ${RTI_JAVA_LIB_PATH}/..:${RTI_JAVA_LIB_PATH}:${RTI_LIB_PATH}
       endif
 
-      # On Ubuntu, disable the new dtags to ensure RPATH is used and not RUNPATH.
+      # On Ubuntu/Debian, disable the new dtags to ensure RPATH is used and not RUNPATH.
       ifneq ("$(wildcard /etc/lsb-release)","")
-         TRICK_USER_LINK_LIBS += -Wl,--disable-new-dtags
+         ifneq (,$(findstring Ubuntu, $(shell grep DISTRIB_ID /etc/lsb-release | cut -d= -f2)))
+            TRICK_USER_LINK_LIBS += -Wl,--disable-new-dtags
+         endif
       endif
 
    else ifeq ($(RTI_VENDOR),Pitch_HLA_Evolved)
@@ -421,9 +423,11 @@ else
          export LD_LIBRARY_PATH = ${RTI_JAVA_LIB_PATH}/..:${RTI_JAVA_LIB_PATH}:${RTI_LIB_PATH}
       endif
 
-      # On Ubuntu, disable the new dtags to ensure RPATH is used and not RUNPATH.
+      # On Ubuntu/Debian, disable the new dtags to ensure RPATH is used and not RUNPATH.
       ifneq ("$(wildcard /etc/lsb-release)","")
-         TRICK_USER_LINK_LIBS += -Wl,--disable-new-dtags
+         ifneq (,$(findstring Ubuntu, $(shell grep DISTRIB_ID /etc/lsb-release | cut -d= -f2)))
+            TRICK_USER_LINK_LIBS += -Wl,--disable-new-dtags
+         endif
       endif
 
    else ifeq ($(RTI_VENDOR),MAK_HLA_Evolved)
