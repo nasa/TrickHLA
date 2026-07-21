b3dd87ee91b7623a496307b5686b797250acbc7d
Dan Dexter <dan.e.dexter@nasa.gov>
2026-07-21 17:38:09 -0500

    Checking for TMM alloced variable.

--- source/TrickHLA/Object.cpp
+++ source/TrickHLA/Object.cpp
@@ -196,7 +196,8 @@ Object::~Object()
       // blocking threads go.
       set_to_unblocking_cyclic_reads();
 
-      if ( this->thread_ids_array != NULL ) {
+      if ( ( this->thread_ids_array != NULL )
+            && trick_MM->is_alloced( static_cast< void * >( this->thread_ids_array ) ) ) {
          if ( trick_MM->delete_var( static_cast< void * >( this->thread_ids_array ) ) ) {
             message_publish( MSG_WARNING, "Object::~Object():%d WARNING failed to delete Trick Memory for 'this->thread_ids_array'\n",
                              __LINE__ );
